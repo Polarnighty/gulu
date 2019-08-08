e28c41396ba5fd0c5528b5a86ab45a552de27222
polarnight <1041465280@qq.com>
2019-08-08 23:49:04 +0800

    添加新组件

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import button from './button'
 import icon from './icon'
 import buttonGroup from './button-group'
 import input from './input'
+import collapse from './collapse'
+import collapseitem from './collapse-item'
 
 
 Vue.component('g-button', button)
 Vue.component('g-icon', icon)
 Vue.component('g-button-group', buttonGroup)
 Vue.component('g-input', input)
+Vue.component('g-collapse', collapse)
+Vue.component('g-collapse-item', collapseitem)
 
 new Vue({
     el: '#app',
---
+++ src/collapse-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="collapseitem">
+    <div class="title">{{title}}</div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+.collapseitem {
+  > .title {
+    border: 1px solid red;
+    border-radius: $border-radius;
+  }
+}
+</style>
+
