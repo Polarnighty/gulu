8bd37e2d0ac05817c80a1a761a5f5e3a2b6d8e58
polarnight <1041465280@qq.com>
2019-08-11 22:56:02 +0800

    修复了bug

---
+++ src/col.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="col" :class="[`col-${span}`,`offset && offset-${offset}`]"
+         :style="{paddingLeft: this.gutter / 2 + 'px',paddingRight: this.gutter / 2 + 'px'
+}"
+    >
+        <div style="border: 1px solid red;height: 100px;">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "g-col",
+        props: {
+            span: {
+                type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+
+    .col {
+        height: 100px;
+        width: 50%;
+        /*border: 1px solid red;*/
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) *100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) *100%;
+            }
+        }
+    }
+</style>
