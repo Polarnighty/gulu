a0246160c01393fac964ef1b1d5fd76938c98fe5
polarnight <1041465280@qq.com>
2019-08-14 23:29:14 +0800

    支持message加入html标签和多行文字等功能

---
+++ src/plugin.js
@@ -0,0 +1,18 @@
+import Toast from './toast'
+
+export default {
+    install(Vue, options) {
+        Vue.prototype.$toast = function (message,toastOptions) {
+            let Constructor = Vue.extend(Toast)
+            let toast = new Constructor({
+                propsData: toastOptions
+                //     {
+                //     closeButton: toastOptions.closeButton
+                // }
+            })
+            toast.$slots.default = [message]
+            toast.$mount()
+            document.body.appendChild(toast.$el)
+        }
+    }
+}
---
+++ src/toast.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="toast" ref="wrapper">
+        <div class="message">
+            <slot v-if="!enableHtml"></slot>
+            <div v-else v-html="$slots.default[0]"></div>
+        </div>
+
+        <div class="line" ref="line"></div>
+
+        <span class="close" v-if="closeButton" @click="onClickClose()">
+            {{closeButton.text}}
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "toast",
+        props: {
+            autoClose: {
+                type: Boolean,
+                default: true
+            },
+            autoCloseDelay: {
+                type: Number,
+                default: 500
+            },
+            closeButton: {
+                type: Object,
+                default: () => {
+                    return {
+                        text: '关闭',
+                        callback: (toast) => {
+                            toast.close()
+                        }
+                    }
+                }
+            },
+            enableHtml: {
+                type: Boolean,
+                default: false
+            }
+        },
+        mounted() {
+            this.execAutoClose()
+            //异步调试大法（时间差）
+            this.$nextTick(() => {
+                this.$refs.line.style.height =
+                    `${this.$refs.wrapper.getBoundingClientRect().height}px`
+            })
+        },
+        methods: {
+            execAutoClose() {
+                if (this.autoClose) {
+                    setTimeout(() => {
+                        this.close()
+                    }, this.autoCloseDelay * 1000)
+                }
+            },
+            close() {
+                this.$el.remove()
+                this.$destroy
+            },
+            onClickClose() {
+                this.close()
+                this.closeButton.callback()
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0, 0, 0, 0.75);
+    .toast {
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: $font-size;
+        line-height: 1.8;
+        min-height: $toast-height;
+        display: flex;
+        align-items: center;
+        color: white;
+        background: $toast-bg;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0px rgba(0, 0, 0, 0.50);
+        padding: 0 16px;
+
+        .message{
+            padding: 8px 0 ;
+        }
+        .close {
+            padding-left: 16px;
+            flex-shrink: 0;
+        }
+
+        .line {
+            border-left: 1px solid #666;
+            height: 100%;
+            margin-left: 16px;
+        }
+    }
+
+
+</style>
