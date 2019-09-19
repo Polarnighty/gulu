6d82c65c445a490ede4d3a87a65ea81752aebd80
polarnight <1041465280@qq.com>
2019-09-19 23:12:47 +0800

    官网部署

--- src/button-group.vue
+++ src/button-group.vue
@@ -18,6 +18,7 @@ export default {
 </script>
 
 <style lang="scss" scope>
+  @import "var";
 .g-button-group {
   display: inline-flex;
   vertical-align: top;
--- src/button.vue
+++ src/button.vue
@@ -1,100 +1,98 @@
 <template>
-    <!-- $emit触发当前组件事件 -->
-    <button class="g-button" :class="{[`icon-${iconPosition}`] : true}" @click="$emit('click')">
-        <g-icon class="icon" v-if="icon  && !loading" :name="icon"></g-icon>
-        <g-icon class="loading icon" v-if="loading " name="loading"></g-icon>
-        <div class="content">
-            <slot></slot>
-        </div>
-    </button>
+  <!-- $emit触发当前组件事件 -->
+  <button class="g-button" :class="{[`icon-${iconPosition}`] : true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon  && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading " name="loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-    import icon from "./icon";
-
-    export default {
-        // props: ["icon", "iconPosition"]
-        components: {
-            "g-icon": icon
-        },
-        props: {
-            icon: {},
-            loading: {
-                type: Boolean,
-                default: false
-            },
-            iconPosition: {
-                type: String, //传一个字符串
-                default: "left",
-                validator(value) {
-                    console.log(value);
-                    if (value !== "left" && value !== "right") {
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
-            }
+import icon from "./icon";
+export default {
+  // props: ["icon", "iconPosition"]
+  components: {
+    "g-icon": icon
+  },
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String, //传一个字符串
+      default: "left",
+      validator(value) {
+        console.log(value);
+        if (value !== "left" && value !== "right") {
+          return false;
+        } else {
+          return true;
         }
-    };
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scope>
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+@import "var";
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.g-button {
+  font-size: $font-size;
+  height: $button-height;
+  padding: 0 1em;
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: $border-color-hover;
+  }
+  &:active {
+    background-color: $button-active-bg;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
     }
 
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
-
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
-        }
-
-        > .icon {
-            order: 1;
-            margin-right: 0.1em;
-        }
-
-        > .content {
-            order: 2;
-        }
-
-        &.icon-right {
-            > .content {
-                order: 1;
-            }
-
-            > .icon {
-                order: 2;
-                margin-right: 0;
-                margin-left: 0.1em;
-            }
-        }
-
-        .loading {
-            animation: spin 2s infinite linear;
-        }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
     }
+  }
+
+  .loading {
+    animation: spin 2s infinite linear;
+  }
+}
 </style> 
